d7f2b9821255aebf83e27a5e8341f00a5b75df8e
Gavin Kashmark <gkashmark@gmail.com>
2024-01-17 15:29:06 -0600

    GK_pizza orders view and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Welcome } from "./components/Welcome/Welcome.js";
+import React from "react";
+import { Outlet, Routes, Route } from "react-router-dom";
+import { EmployeeNav } from "./components/nav/EmployeeNav.js";
+import { OrderList } from "./components/Orders/OrderList.js";
 
-function App() {
-  return <div className="App">Shepherd's Pies</div>;
-}
+export const App = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <EmployeeNav />
+            <Outlet />
+          </>
+        }
+      />
+      <Route index element={<Welcome />} />
+      <Route path="orders" element={<OrderList />} />
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/components/nav/EmployeeNav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Nav.css";
+import { Link, useNavigate } from "react-router-dom";
+
+export const EmployeeNav = () => {
+  const navigate = useNavigate();
+  return (
+    <ul className="navbar">
+      <li className="navbar-item text-primary">
+        <Link to="/create">Create Order!</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/orders">All Orders</Link>
+      </li>
+      {localStorage.getItem("pizza_user") ? (
+        <li className="navbar-item navbar-logout">
+          <Link
+            className="navbar-link"
+            to=""
+            onClick={() => {
+              localStorage.removeItem("pizza_user");
+              navigate("/", { replace: true });
+            }}
+          >
+            Logout
+          </Link>
+        </li>
+      ) : (
+        ""
+      )}
+    </ul>
+  );
+};
