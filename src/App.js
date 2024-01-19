afede8b5d76ad60a4658c2a2f212990c04c3b8e5
Stacy Griffy <sgriffy2@gmail.com>
2024-01-19 09:57:26 -0600

    Created createOrder component and styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import { Outlet, Routes, Route } from "react-router-dom";
 import { EmployeeNav } from "./components/nav/EmployeeNav.js";
 import { OrderList } from "./components/Orders/OrderList.js";
+import { CreateOrder } from "./components/Orders/CreateOrder.js";
 
 export const App = () => {
   return (
@@ -19,6 +20,8 @@ export const App = () => {
       />
       <Route index element={<Welcome />} />
       <Route path="orders" element={<OrderList />} />
+      <Route path="createOrder" element={<CreateOrder />} />
+      <Route path="createPizza" />
     </Routes>
   );
 };
--- src/Services/orderServices.js
+++ src/Services/orderServices.js
@@ -1,3 +1,13 @@
 export const getAllOrders = () => {
   return fetch("http://localhost:8088/orders").then((res) => res.json());
 };
+
+export const addNewOrder = (orderObj) => {
+  return fetch(`http://localhost:8088/orders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(orderObj),
+  });
+};
